dcdc1245bd391975fc0f827ad6e17d8b06ed7618
JRAllum <allum434@gmail.com>
2019-07-25 11:51:55 +0200

    Initial commit

---
+++ ObligatoriskOppgave_1/Person.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace ObligatoriskOppgave_1
+{
+    public class Person
+    {
+        public int BirthYear;
+
+        public string LastName;
+
+        public string FirstName;
+
+        public int Id;
+
+        public Person Mother;
+
+        public Person Father;
+
+
+    }
+
+}
